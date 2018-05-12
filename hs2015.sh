4293ef77042c125b6131e23e170e84b648b4012f
Arne Neumann <github@arne.cl>
2018-05-12 18:03:24 +0200

    wrapper script now produces output file

--- hs2015.sh
+++ hs2015.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/local/bin/rst_parse --segmentation_model segmentation_model.C32.0 --parsing_model rst_parsing_model.C0.5 $1
+/usr/local/bin/rst_parse --segmentation_model segmentation_model.C32.0 --parsing_model rst_parsing_model.C0.5 $1 > $1.hs2015
+cat $1.hs2015
